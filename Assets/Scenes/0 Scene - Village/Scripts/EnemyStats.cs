e94cb634b7a8838de431fe14ac4fc6d11b828cdf
YaGi4 <ebahtin6@gmail.com>
2023-04-07 12:50:14 +0300

    создал почти первую карту

---
+++ Assets/Scenes/0 Scene - Village/Scripts/EnemyStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float healthPoints = 74f;
+    public float maxHP = 100f;
+    public float damage = 25f;
+    public float attackRange = 10f;
+    public float armor = 5f;
+    public float speedAttack = 1f;
+}
