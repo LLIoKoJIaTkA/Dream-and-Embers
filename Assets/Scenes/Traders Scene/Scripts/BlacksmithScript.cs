aa9b535df5b717d5a65a88087079ed5f81674e80
iehoell <sashaotmachov@gmail.com>
2023-04-12 21:22:01 +0300

    Исправил ошибки в скрипте

--- Assets/Scenes/Traders Scene/Scripts/BlacksmithScript.cs
+++ Assets/Scenes/Traders Scene/Scripts/BlacksmithScript.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BlacksmithScript : MonoBehaviour
 {
     [SerializeField] GameObject IterationWindow;
 
-    private bool workIsActive;
+    private bool _IsWorkActive;
 
     void Start()
     {
@@ -15,23 +13,22 @@ public class BlacksmithScript : MonoBehaviour
 
     private void OnTriggerStay2D()
     {
-        workIsActive = true;
-        if(Input.GetKey(KeyCode.E))
+        _IsWorkActive = true;
+        if (Input.GetKey(KeyCode.E))
         {
             IterationWindow.SetActive(true);
             Time.timeScale = 0;     //freeze time, when hero use blacksmith menu
-        }
-        
+        }    
     }
 
     private void Update()
     {
-        if(workIsActive == true)
+        if (_IsWorkActive)
         {
-            if(Input.GetKey(KeyCode.Escape))
+            if (Input.GetKey(KeyCode.Escape))
             {
                 IterationWindow.SetActive(false);
-                workIsActive = false;
+                _IsWorkActive = false;
                 Time.timeScale = 1;     //unfreeze time, when hero use blacksmith menu
             }
         }
